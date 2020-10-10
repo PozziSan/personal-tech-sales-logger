024cdba89dd1b34a50775ad4618ebdc90c9338b4
Pedro Pozzi Ferreira <pedropozzif@gmail.com>
2020-10-09 23:08:51 -0300

    Add Heroku Deploy Configurations to settings.py

--- tech_sales_logger/tech_sales_logger/settings.py
+++ tech_sales_logger/tech_sales_logger/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://tech-sales-logger.herokuapp.com']
 
 # Application definition
 
@@ -117,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = os.path.join(BASE_DIR, 'staticfile/')
+
+django_heroku.settings(locals())
