61c811de599764a2b287b9343fce34c7ed2cfd93
Pedro Pozzi Ferreira <pedropozzif@gmail.com>
2020-10-09 23:55:30 -0300

    Add Admin Dashboard Template

--- tech_sales_logger/tech_sales_logger/settings.py
+++ tech_sales_logger/tech_sales_logger/settings.py
@@ -51,11 +51,12 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'tech_sales_logger.urls'
+TEMPLATE_DIR = os.path.join(BASE_DIR, "tech_sales_logger/templates")
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
